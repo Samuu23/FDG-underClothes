9a78ecac006efc3ed16ad1f703d9366c31b8d3df
Samuu23 <samuelarzelan18@gmail.com>
2022-04-25 22:47:24 -0300

    sincronizando router 2

--- src/Components/ItemCheckout.jsx
+++ src/Components/ItemCheckout.jsx
@@ -1,9 +1,10 @@
 import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const ItemCheckout = ()=> {
     return(
         <>
-        <Button variant="contained" color="success" size="large">Checkout</Button>
+        <Link to="/Cart"><Button variant="contained" color="success" size="large">Checkout</Button></Link>
         </>
     )
 }
